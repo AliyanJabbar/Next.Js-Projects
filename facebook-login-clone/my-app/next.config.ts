9e2ac986113ebbe432ba58569c86d6fa0ebdbf6d
aliyan jabbar <jabbaraliyan805@gmail.com>
2024-11-16 23:43:51 +0500

    clearing that it is a demo project to avoid dangerous site flag given by browser

--- facebook-login-clone/my-app/next.config.ts
+++ facebook-login-clone/my-app/next.config.ts
@@ -1,8 +1,26 @@
 import type { NextConfig } from "next";
 
-const nextConfig:NextConfig = {
-  eslint:{
-      ignoreDuringBuilds:true,
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      }
+    ];
   }
 };
+
 export default nextConfig;
